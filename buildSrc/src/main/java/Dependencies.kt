80ca7df6169c797864f9f7408f65526b13dfb144
David Khol <david.khol@ackee.cz>
2019-03-18 16:29:37 +0100

    Added new context injection.

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -3,7 +3,7 @@ object ApplicationId {
 }
 
 object Releases {
-    const val versionCode = 8
+    const val versionCode = 11
     const val versionName = "1.0"
 }
 
--- feature/borrower/src/main/java/me/khol/borrower/BorrowerActivity.kt
+++ feature/borrower/src/main/java/me/khol/borrower/BorrowerActivity.kt
@@ -1,9 +1,16 @@
 package me.khol.borrower
 
+import android.content.Context
 import android.os.Bundle
+import com.google.android.play.core.splitcompat.SplitCompat
 import me.khol.base.base.BaseActivity
 
 class BorrowerActivity : BaseActivity() {
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        SplitCompat.install(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
